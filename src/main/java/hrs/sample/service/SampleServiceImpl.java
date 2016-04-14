12239f2888b586e59d0ff26b1df81b5452ac7b00
Gwangwoo Kim <gwkim8107@tourbaksa.com>
2016-04-14 19:11:04 +0900

    commit test3

--- src/main/java/hrs/sample/service/SampleServiceImpl.java
+++ src/main/java/hrs/sample/service/SampleServiceImpl.java
@@ -9,7 +9,7 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import hrs.sample.dao.SampleDAO;
-
+//Cotroller 에 등록한 sampleService 를 Service Bean 으로 등록한다.
 @Service("sampleService")
 public class SampleServiceImpl implements SampleService{
 	Logger log = Logger.getLogger(this.getClass());
@@ -18,8 +18,8 @@ public class SampleServiceImpl implements SampleService{
 	private SampleDAO sampleDAO;
 	
 	@Override
-	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
-		return sampleDAO.selectBoardList(map);
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> commandMap) throws Exception {
+		return sampleDAO.selectBoardList(commandMap);
 		
 	}
 
